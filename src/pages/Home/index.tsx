6284a1644257e85b95e03676d06dfb8781f988c5
Ylan Dahan Benoliel Silva <ylan.d@havan.com.br>
2022-06-03 22:58:55 -0300

    Show loaded products on page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -52,23 +52,25 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {products.map((item) => (
+        <li>
+          <img src={item.image} alt={item.title} />
+          <strong>{item.title}</strong>
+          <span>{item.priceFormatted}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            // onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {/* {cartItemsAmount[product.id] || 0} */} 2
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 };
