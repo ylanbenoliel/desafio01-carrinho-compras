79ef60f64fc40bb3cd31a7355c1f2f50799d73d3
Ylan Dahan Benoliel Silva <ylan.d@havan.com.br>
2022-06-09 23:11:36 -0300

    Add remove product button feature

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -40,7 +40,7 @@ const Cart = (): JSX.Element => {
   }
 
   function handleRemoveProduct(productId: number) {
-    // TODO
+    removeProduct(productId);
   }
 
   return (
@@ -97,7 +97,7 @@ const Cart = (): JSX.Element => {
                 <button
                   type="button"
                   data-testid="remove-product"
-                  // onClick={() => handleRemoveProduct(product.id)}
+                  onClick={() => handleRemoveProduct(product.id)}
                 >
                   <MdDelete size={20} />
                 </button>
